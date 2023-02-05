71b68676f087f720a6668528bf0d2aede7c62bbe
zara-khan123 <106547199+zara-khan123@users.noreply.github.com>
2023-02-05 11:25:11 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ function preload()
 function setup(){
     canvas = createCanvas(300, 300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw()
@@ -14,4 +20,23 @@ function draw()
 function take_snapshot()
 {
     save('myFilterImage.png');
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("mouth x = " + results[0].pose.mouth.x);
+        console.log("mouth y = " + results[0].pose.mouth.y);
+    } else{
+        console.log(results);
+        console.log("mouth x = " + results[0].pose.mouth.x);
+        console.log("mouth y = " + results[0].pose.mouth.y);
+    }
 }
